4703d891d7c2628fe9870e01b73029626e328f3a
grimaldo.soto <Grimaldo.SOTO@Outlook.com>
2023-09-07 21:30:39 -0500

    Potenciando el GenericRepository Part 1

--- POS.Infrastructure/Persistence/Contexts/Configurations/CategoryConfiguration.cs
+++ POS.Infrastructure/Persistence/Contexts/Configurations/CategoryConfiguration.cs
@@ -8,6 +8,9 @@ public class CategoryConfiguration : IEntityTypeConfiguration<Category>
 {
     public void Configure(EntityTypeBuilder<Category> builder)
     {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id)
+            .HasColumnName("CategoryId");
         builder.Property(e => e.Name).HasMaxLength(100);
     }
 }
--- POS.Infrastructure/Persistence/Interfaces/IGenericRepository.cs
+++ POS.Infrastructure/Persistence/Interfaces/IGenericRepository.cs
@@ -1,6 +1,19 @@
+using System.Linq.Expressions;
+using POS.Domain.Entities;
+using POS.Infrastructure.Commons.Bases.Request;
+
 namespace POS.Infrastructure.Persistence.Interfaces;
 
-public interface IGenericRepository<T> where T : class
+public interface IGenericRepository<T> where T : BaseEntity
 {
-    
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<bool> RegisterAsync(T entity);
+    Task<bool> EditAsync(T entity);
+    Task<bool> RemoveAsync(int id);
+    IQueryable<T> GetEntityQuery(Expression<Func<T, bool >>? filter = null );
+
+    IQueryable<TDto> Ordering<TDto>(BasePaginationRequest request, IQueryable<TDto> queryable, bool pagination = false)
+        where TDto : class;
+
 }
